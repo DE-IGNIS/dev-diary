0a5c3b661dea16c760121e8881ac3e0ae504e108
DE-IGNIS <chauhansudarsh@gmail.com>
2026-03-24 21:52:53 +0530

    Add : created dummy route for blog feature

---
+++ app/api/blog/route.ts
@@ -0,0 +1,10 @@
+export async function GET() {
+  return new Response(
+    JSON.stringify({
+      success: true,
+    }),
+    {
+      headers: { "Content-Type": "application/json" },
+    },
+  );
+}
