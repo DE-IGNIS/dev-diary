569563e0f718ca6d250d85aa58287d77a9798cb7
DE-IGNIS <chauhansudarsh@gmail.com>
2026-03-24 18:18:10 +0530

    Fix : Landing pages for generic routes

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>This is the About Page</h1>
+
+      <p>The about page would contain important information about the website</p>
+    </div>
+  );
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+export default function ContactPage() {
+  return (
+    <div>
+      <h1>This is a Contact Page</h1>
+      <label>
+        Enter Email
+        <input placeholder="example@gmail.com" />
+      </label>
+      <button type="submit">Submit</button>
+    </div>
+  );
+}
